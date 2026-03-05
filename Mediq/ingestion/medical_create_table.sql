368b6d4815b10467239e3b9fa62e7c865668a597
deepakkumar2204ca <deepak.kumar2204ca@cognizant.com>
2026-03-05 15:12:55 +0530

    Delete Mediq/ingestion directory

--- Mediq/ingestion/medical_create_table.sql
+++
@@ -1,7 +0,0 @@
---This table contain data for disease name and test name
-CREATE SCHEMA IF NOT EXISTS :"schema_name";
-CREATE TABLE IF NOT EXISTS :"schema_name".disease_tests_info (
-  disease_name_csv        TEXT NOT NULL,
-  disease_name            TEXT NOT NULL,
-  test_name               TEXT
-);
